6ac74a8c1ad4c9cdade60b900e2ffdc6fe1036af
Deng, Daisy <daisy.deng@intel.com>
2024-11-22 02:09:26 +0000

    Revert "remove unused function get_backend_ops as design changes, return torch.autocast in get_gpu_autocast"
    This reverts commit b29b5934d3fd9477f7f1cc2203fda8c39782a8b6.

--- test/test_ops.py
+++ test/test_ops.py
@@ -79,7 +79,6 @@ from torch.testing._internal.common_utils import (
     unMarkDynamoStrictTest,
     GPU_TYPES,
     HAS_GPU,
-    get_gpu_autocast,
 )
 from torch.utils._python_dispatch import TorchDispatchMode
 from torch.utils._pytree import tree_map
@@ -2840,7 +2839,7 @@ class TestFakeTensor(TestCase):
         fake_backward_xfails | fake_autocast_backward_xfails,
     )
     def test_fake_crossref_backward_amp(self, device, dtype, op):
-        self._test_fake_crossref_helper(device, dtype, op, get_gpu_autocast())
+        self._test_fake_crossref_helper(device, dtype, op, torch.cuda.amp.autocast)
 
     @ops([op for op in ops_and_refs if op.is_factory_function])
     def test_strided_layout(self, device, dtype, op):
